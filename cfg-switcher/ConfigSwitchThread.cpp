668d447f78e286bd2fa304a951858b53ae55a4f5
cspangler <connor.spangler.1@us.af.mil>
2018-09-17 18:23:09 -0700

    Began implementing separate config switch thread

---
+++ cfg-switcher/ConfigSwitchThread.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <string>
+#include "ConfigSwitchThread.h"
+#include "WinUtils.h"
+
+unsigned int __stdcall configSwitchThread(void*) {
+	return 0;
+}
--- cfg-switcher/main.cpp
+++ cfg-switcher/main.cpp
@@ -4,11 +4,16 @@
 #include <Windows.h>
 #include <process.h>
 #include "PowerWindowThread.h"
+#include "ConfigSwitchThread.h"
 #include "CfgSwitchAPI.h"
 
+const int NUM_THREADS = 2;
+
 int main() {
-	std::atomic<HWND> hiddenWindowHandle(NULL); 
-	HANDLE PowerWindowThreadHandle = (HANDLE)_beginthreadex(0, 0, &windowsPowerThread, (void*)&hiddenWindowHandle, 0, 0);
+	std::atomic<HWND> hiddenWindowHandle(NULL);
+	HANDLE ThreadHandles[NUM_THREADS];
+	ThreadHandles[0] = (HANDLE)_beginthreadex(0, 0, &windowsPowerThread, (void*)&hiddenWindowHandle, 0, 0);
+	ThreadHandles[1] = (HANDLE)_beginthreadex(0, 0, &configSwitchThread, 0, 0, 0);
 	while(hiddenWindowHandle == NULL);
 
 	if (!initSettings())
@@ -19,8 +24,7 @@ int main() {
 
 	SendMessage(hiddenWindowHandle, WM_DESTROY, NULL, NULL);
 
-	WaitForSingleObject(PowerWindowThreadHandle, INFINITE);
-	CloseHandle(PowerWindowThreadHandle);
+	WaitForMultipleObjects(NUM_THREADS, ThreadHandles, TRUE, INFINITE);
 
 	deleteSettingsFile();
 
