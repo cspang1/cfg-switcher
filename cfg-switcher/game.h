4c1d6c44262cd189acee2160df8dbe38fa7e4b41
cspangler <connor.spangler.1@us.af.mil>
2018-09-28 09:30:42 -0700

    Implemented QDataStream Game overloaded operators

--- cfg-switcher/game.h
+++ cfg-switcher/game.h
@@ -3,32 +3,39 @@
 
 #include <QString>
 #include <QMetaType>
+#include <QDataStream>
 
 class Game {
 public:
     QString ID;
     QString cfgPath;
-	bool mainCfgSet;
-	bool battCfgSet;
+    bool mainCfgSet;
+    bool battCfgSet;
     bool enabled;
     Game& operator=(const Game& other) {
-		ID = other.ID;
-		cfgPath = other.cfgPath;
-		mainCfgSet = other.mainCfgSet;
-		battCfgSet = other.battCfgSet;
-        enabled = other.enabled;
-		return *this;
-	}
-    Game(const Game& other) {
         ID = other.ID;
         cfgPath = other.cfgPath;
         mainCfgSet = other.mainCfgSet;
         battCfgSet = other.battCfgSet;
         enabled = other.enabled;
+        return *this;
     }
+    Game(const Game& other) :
+        ID(other.ID), cfgPath(other.cfgPath), mainCfgSet(other.mainCfgSet), battCfgSet(other.battCfgSet), enabled(other.enabled) {}
     Game() : ID(""), cfgPath(""), mainCfgSet(false), battCfgSet(false), enabled(false) {}
-    Game(QString _ID, QString _cfgPath, bool _mainCfgSet = false, bool _battCfgSet = false, bool _enabled = false)
-        :ID(_ID), cfgPath(_cfgPath), mainCfgSet(_mainCfgSet), battCfgSet(_battCfgSet), enabled(_enabled) { }
+    Game(QString _ID, QString _cfgPath, bool _mainCfgSet = false, bool _battCfgSet = false, bool _enabled = false) :
+        ID(_ID), cfgPath(_cfgPath), mainCfgSet(_mainCfgSet), battCfgSet(_battCfgSet), enabled(_enabled) { }
+
+private:
+    friend QDataStream& operator << (QDataStream& out, const Game& game) {
+        out << game.ID << game.cfgPath << game.mainCfgSet << game.battCfgSet << game.enabled;
+        return out;
+    }
+    friend QDataStream& operator >> (QDataStream& in, Game& game) {
+        in >> game.ID >> game.cfgPath >> game.mainCfgSet >> game.battCfgSet >> game.enabled;
+        return in;
+    }
+
 };
 
 Q_DECLARE_METATYPE(Game);
